cccc1be7a2d13f673bc2e5e6b8e4c1808a837696
Matthew Young <matthew.young@unh.edu>
2021-06-09 22:38:38 -0400

    Add parameter descriptional to main docstring

--- demo.py
+++ demo.py
@@ -6,7 +6,28 @@ def main(
     str_arg: str,
     float_kwarg: float=1.0,
 ) -> None:
-    """Print the user values."""
+    """Print the user values.
+    
+    Positional Parameters
+    ---------------------
+    int_arg : int
+
+    an integer positional argument
+
+    str_arg : str
+
+    a string positional argument
+
+    Keyword Parameters
+    ------------------
+    float_kwarg : float
+
+    a float keyword argument
+
+    Return
+    ------
+    None
+    """
     print(f"{int_arg=}")
     print(f"{str_arg=}")
     print(f"{float_kwarg=}")
