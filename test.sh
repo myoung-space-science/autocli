e26d3a2d9da44ced76d61a7e0f24bdb95cffb17f
Matthew Young <matthew.young@unh.edu>
2021-06-09 22:43:10 -0400

    Rename reference to base for easier comparison

--- test.sh
+++ test.sh
@@ -4,16 +4,16 @@ int_arg=1
 str_arg=cat
 float_kwarg=2.3
 
-echo "REFERENCE HELP"
-python ./reference.py -h
+echo "BASE HELP"
+python ./base.py -h
 
 echo
 echo "DEMO HELP"
 python ./demo.py -h
 
 echo
-echo "REFERENCE RESULT"
-python ./reference.py ${int_arg} ${str_arg} --float_kwarg ${float_kwarg}
+echo "BASE RESULT"
+python ./base.py ${int_arg} ${str_arg} --float_kwarg ${float_kwarg}
 
 echo
 echo "DEMO RESULT"
